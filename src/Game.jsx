eb071a2ab9799f6fcc4755d012caf8b47cebde4d
Framzy <fardenramzy97@gmail.com>
2025-10-14 18:27:18 +0700

    Last update

--- src/Game.jsx
+++ src/Game.jsx
@@ -28,7 +28,7 @@ function Board({ xIsNext, squares, onPlay }) {
 
   return (
     <>
-      <div className="status mb-4 font-bold text-[#ffffff]">{status}</div>
+      <div className="status mb-4 font-bold text-[#d7d7d7]">{status}</div>
       <div className="board grid grid-cols-3 gap-1">
         {squares.map((square, index) => (
           <Square
@@ -80,16 +80,27 @@ export default function Game() {
   });
 
   return (
-    <div className="game flex flex-row justify-center items-center gap-10 h-screen bg-[#2f7078]">
-      <div className="game-board text-center">
-        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+    <>
+      <div className="bg-[#2f7078] h-screen flex flex-col justify-center items-center">
+        <h1 className="text-[#e1e1e1] text-center text-4xl font-bold mb-10 shadow-md">
+          Tic Tac Toe Game
+        </h1>
+        <div className="game flex flex-row justify-center items-center gap-10 ">
+          <div className="game-board text-center">
+            <Board
+              xIsNext={xIsNext}
+              squares={currentSquares}
+              onPlay={handlePlay}
+            />
+          </div>
+          <div className="game-info bg-[#1f4347] flex  justify-center w-55 min-h-65 max-h-65 overflow-y-auto p-4 border-2 shadow-md ">
+            <ol className="list-decimal list-inside space-y-2 text-[#e4e4e4] ">
+              {moves}
+            </ol>
+          </div>
+        </div>
       </div>
-      <div className="game-info bg-[#1f4347] flex  justify-center w-55 min-h-65 max-h-65 overflow-y-auto p-4 border-2 shadow-md ">
-        <ol className="list-decimal list-inside space-y-2 text-[#ffffff] ">
-          {moves}
-        </ol>
-      </div>
-    </div>
+    </>
   );
 }
 
